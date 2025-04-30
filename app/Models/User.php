b74c16c44d53eb4e55930d70af640b52ce395349
Maxime Nicolas <nico.maxime84@gmail.com>
2025-04-30 15:35:34 +0200

    Avancement du projet

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Enums\Role;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -14,9 +16,9 @@ class User extends Authenticatable
 
     /**
      * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    *
+    * @var array<int, string>
+    */
     protected $fillable = [
         'name',
         'email',
@@ -25,9 +27,9 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
+    *
+    * @var array<int, string>
+    */
     protected $hidden = [
         'password',
         'remember_token',
@@ -35,10 +37,16 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+    *
+    * @var array<string, string>
+    */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'role' => Role::class,
     ];
+
+    public function isAdmin(): bool
+    {
+        return $this->role === Role::Admin;
+    }
 }
